3e3cc01df7acb57a8d4ee9ba058206d7a8cba1a3
Jakub Stefko <jakub.stefko@itti.com.pl>
2018-11-19 01:45:41 +0100

    works fine

--- astar.js
+++ astar.js
@@ -1,5 +1,5 @@
 var aStarError = document.getElementById('aStarError')
-var matrix, closedList, queue
+var matrix, closedList, queue, score = [], isFound = false
 
 const createField = (size) => {
   console.log('%c *** MATRIX CREATE ***', 'color: white; background: black;')
@@ -36,31 +36,55 @@ const aStarSearch = (startX, startY, endX, endY) => {
     let node
     do {
       node = queue.delete()
+      score.push(node)
       if (node.x === endX && node.y === endY) {
+        console.log('%c *** FOUND ***', 'color: black; background: yellow;')
+        isFound = true
         break // we found the right path!
       } else {
         closedList.table[node.x][node.y] = node.priority
         console.log('node', node)
         if (((node.x - 1) >= Math.min(startX, endX)) && (matrix.table[node.x - 1][node.y] !== 'x')) {
-          console.log('cool [x-1][y]')
           vCheck(node.x - 1, node.y, node)
-        } else { console.log('out! [x-1][y]') }
-        if ((node.x + 1) < Math.max(startX, endX) && (matrix.table[node.x + 1][node.y] !== 'x')) {
-          console.log('cool [x+1][y]')
+        }
+        if ((node.x + 1) <= Math.max(startX, endX) && (matrix.table[node.x + 1][node.y] !== 'x')) {
           vCheck(node.x + 1, node.y, node)
-        } else { console.log('out! [x+1][y]') }
-        if ((node.y - 1) > Math.min(startY, endY) && (matrix.table[node.x][node.y - 1] !== 'x')) {
-          console.log('cool [x][y-1]')
+        }
+        if ((node.y - 1) >= Math.min(startY, endY) && (matrix.table[node.x][node.y - 1] !== 'x')) {
           vCheck(node.x, node.y - 1, node)
-        } else { console.log('out! [x][y-1]') }
+        }
         if ((node.y + 1) <= Math.max(startY, endY) && (matrix.table[node.x][node.y + 1] !== 'x')) {
-          console.log('cool [x][y+1]')
           vCheck(node.x, node.y + 1, node)
-        } else { console.log('out! [x][y+1]') }
+        }
       }
     } while (!queue.isEmpty())
     console.log('%c *** A STAR END ***', 'color: white; background: teal;')
     closedList.showAll()
+    console.log('END!!!!!!!!!', {score})
+
+    // we've done algorithmic path -> now we just need to find exact one
+
+    // ***
+    if (isFound) {
+      let table = [], i
+      node = score[score.length -1]
+      while (node.x !== startX || node.y !== startY) {
+        i = null
+        table.unshift(node)
+        score.forEach(step => {
+          if (step.x === node.parentX && step.y === node.parentY && !i) {
+            i = step
+          } else if (step.x === node.parentX && step.y === node.parentY && i.priority && step.priority < i.priority) {
+            i = step
+          }
+        })
+        score.unshift(i)
+        node = i
+      }
+      table.unshift(score[0])
+      console.log('EXACT PATH: ', table)
+    } else { console.log('no path avilable!') }
+    // ***
   }
 }
 
@@ -73,10 +97,17 @@ const aStarSearch = (startX, startY, endX, endY) => {
 //  push currentNode onto closedListList and remove from openList
 //  foreach neighbor of currentNode {
 //      if neighbor is not in openList {
-//             save g, h, and f then save the current parent
+//             save priority
 //             add neighbor to openList
 //      }
 //      if neighbor is in openList but the current g is better than previous g {
-//              save g and f, then save the current parent
+//              save priority
 //      }
 //  }
+// *** now we have algorithmic path
+// node = endpoint
+// until node.parent === node { --> only START point scenario
+//   find lowest priority neighbor
+//   save it to the score
+//   it will now be currentNode
+// }
